a6ac10205d412164a8c1a259068bd1231e41c11b
CandyVelazco100 <nohemivelazco3@gmail.com>
2023-04-12 20:10:52 -0700

    nuevos cambios

--- src/App.js
+++ src/App.js
@@ -27,6 +27,109 @@ function App() {
     }
     setSudokuArr(grid);
   }
+
+  function compareSudokus(currentSudoku, solvedSudoku){
+    let res ={
+      isComplete: true,
+      isSolvable: true
+    }
+
+    for(var i = 0; i < 9; i++){
+      for(var j = 0; j < 9; j++){
+        if(currentSudoku[i][j] != solvedSudoku[i][j]){
+          if(currentSudoku[i][j] != -1){
+            res.isSolvable = false;
+          }
+          res.isComplete = false;
+       }
+      }
+    }
+    return res;
+  }
+
+  function checkSudoku(){
+    let sudoku = getDeepCopy(initial);
+    solver(sudoku);
+    let compare = compareSudokus(SudokuArr, sudoku);
+
+    if(compare.isComplete){
+      alert("¡Felicidades! Sudoku resuelto")
+    }else if(compare.isSolvable){
+      alert("Sigue adelante...")
+    }else{
+      alert("Nel, esta mal :(")
+    }
+  }
+
+  function checkRow(grid, row, num){
+    return grid[row].indexOf(num) === -1;
+  }
+
+  function checkCol(grid, col, num){
+    return grid.map(row => row[col]).indexOf(num) === -1;
+  }
+
+  function checkBox(grid, row, col, num){
+    let boxArr = [],
+    rowStart = row - (row%3),
+    colStart = col - (col%3);
+
+    for(let i = 0; i < 3; i++){
+      for(let j = 0; j < 3; j++){
+        boxArr.push(grid[rowStart + i][colStart + j])
+      }
+    }
+    return boxArr.indexOf(num) === -1;
+  }
+
+  function checkValid(grid, row, col, num){
+    if(checkRow(grid, row, num) && checkCol(grid, col, num) && checkBox(grid, row, col, num)){
+      return true;
+    }
+    return false;
+  }
+
+  function getNext(row, col){
+    return col !== 8 ? [row, col + 1] : row != 8 ? [row + 1, 0] : [0, 0];
+  }
+
+  function solver(grid, row=0, col=0){
+    if(grid[row][col]!== -1){
+      let isLast = row >= 8 && col >= 8; 
+      if(!isLast){
+        let [newRow, newCol] = getNext(row, col);
+        return solver(grid, newRow, newCol);
+      }   
+    }
+
+    for(let num = 1; num <=9; num++){
+      if(checkValid(grid, row, col, num)){
+        grid[row][col] = num; 
+        let [newRow, newCol] = getNext(row, col);   
+        
+        if(!newRow && !newCol){
+          return true;
+        }
+
+        if(solver(grid, newRow, newCol)){
+          return true;
+        }
+      }
+    }
+    grid[row][col] = -1;         
+    return false;
+  }
+
+  function solveSudoku(){
+    let sudoku = getDeepCopy(initial);
+    solver(sudoku);
+    setSudokuArr(sudoku);
+  }
+
+  function resetSudoku(){
+    let sudoku = getDeepCopy(initial);
+    setSudokuArr(sudoku);
+  }
   
   return (
     <div className="App">
@@ -53,9 +156,9 @@ function App() {
             </tbody>
         </table>
         <div className="buttonContainer"> 
-          <button className="checkButton">Check</button>
-          <button className="solverButton">Solve</button>
-          <button className="resetButton">Reset</button>
+          <button className="checkButton" onClick={checkSudoku}>Check</button>
+          <button className="solveButton" onClick={solveSudoku}>Solve</button>
+          <button className="resetButton" onClick={resetSudoku}>Reset</button>
 
         </div>
       </div>
